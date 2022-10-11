c8dbea965d6384cb6b67a386158007e762097abb
NigamLad <nigamlad2012@gmail.com>
2022-10-11 15:25:59 -0600

    Bug Fix
    Removed loop causing multiple event listeners per content object

--- js/collapse.js
+++ js/collapse.js
@@ -1,18 +1,16 @@
 $(window).on('load', function () {
     //$("#dataPreview").bind("DOMSubtreeModified", function () {
         var coll = document.getElementsByClassName("collapsible");
-        for (var x = 0; x < coll.length; x++) {
-            for (i = 0; i < coll.length; i++) {
-                coll[i].addEventListener("click", function () {
-                    this.classList.toggle("active");
-                    var content = this.nextElementSibling;
-                    if (content.style.display === "block") {
-                        content.style.display = "none";
-                    } else {
-                        content.style.display = "block";
-                    }
-                });
-            }
+        for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.display === "block") {
+                    content.style.display = "none";
+                } else {
+                    content.style.display = "block";
+                }
+            });
         }
     //})
 })
